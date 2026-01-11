1409f70d530109a556f97b2be390a3398d362877
Siyun Chen <134215502+ChenSiyun1234@users.noreply.github.com>
2026-01-11 07:38:20 -0800

    SVG + Tests Passed + Makefile (without Documentation and Reference)
    gonna add reference and comments later

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -1,83 +1,396 @@
 #include "StringUtils.h"
 #include <cctype>
+#include <algorithm>
 
 namespace StringUtils{
 
 std::string Slice(const std::string &str, ssize_t start, ssize_t end) noexcept{
-    // Replace code here
-    return "";
+    ssize_t strlen = str.size();
+
+    if (strlen == 0)
+    {
+        return "";
+    }
+
+    if (end == 0)
+    {
+        end = strlen;
+    }
+
+    if (start < 0)
+    {
+        start = start + strlen;
+    }
+
+    if (start > strlen) 
+    {
+        start = strlen;
+    }
+
+    if(end < 0){
+        end = end + strlen;
+    }
+
+    if (end > strlen) 
+    {
+        end = strlen;
+    }
+
+    if (end < start)
+    {
+        return "";
+    }
+
+    std::string slice;
+    for(ssize_t i = start; i < end; i ++)
+    {
+        slice += str[i];
+    }
+
+    return slice;
 }
 
 std::string Capitalize(const std::string &str) noexcept{
-    // Replace code here
     std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (strlen == 0)
+    {
+        return "";
+    }
+
+    for (ssize_t i = 1; i < strlen; i ++)
+    {
+        Temp[i] = tolower(Temp[i]);
+    }
+
     Temp[0] = toupper(Temp[0]);
     return Temp;
 }
 
 std::string Upper(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (strlen == 0)
+    {
+        return "";
+    }
+
+    for (ssize_t i = 0; i < strlen; i ++)
+    {
+        Temp[i] = toupper(Temp[i]);
+    }
+
+    return Temp;
 }
 
 std::string Lower(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (strlen == 0)
+    {
+        return "";
+    }
+
+    for (ssize_t i = 0; i < strlen; i ++)
+    {
+        Temp[i] = tolower(Temp[i]);
+    }
+
+    return Temp;
 }
 
 std::string LStrip(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (strlen == 0)
+    {
+        return "";
+    }
+
+    ssize_t start = 0;
+    for (ssize_t i = 0; i < strlen; i ++)
+    {
+        if (isspace(Temp[start]))
+        {
+            start ++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return Slice(Temp, start, strlen);
 }
 
 std::string RStrip(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (strlen == 0)
+    {
+        return "";
+    }
+
+    ssize_t end = strlen;
+    for (ssize_t i = 0; i < strlen; i ++)
+    {
+        if (isspace(Temp[end - 1]))
+        {
+            end --;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return Slice(Temp, 0, end);
 }
 
 std::string Strip(const std::string &str) noexcept{
-    // Replace code here
-    return "";
-}
+    std::string Temp = str;
 
-std::string Center(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
-    return "";
+    ssize_t strlen = str.size();
+
+    if (strlen == 0)
+    {
+        return "";
+    }
+
+    Temp = LStrip(Temp);
+    Temp = RStrip(Temp);
+
+    return Temp;
 }
 
 std::string LJust(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (width <= strlen)
+    {
+        return Temp;
+    }
+
+    for (ssize_t i = 0; i < width - strlen; i ++)
+    {
+        Temp = Temp + fill;
+    }
+
+    return Temp;
 }
 
 std::string RJust(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (width <= strlen)
+    {
+        return Temp;
+    }
+
+    for (ssize_t i = 0; i < width - strlen; i ++)
+    {
+        Temp = fill + Temp;
+    }
+
+    return Temp;
+}
+
+std::string Center(const std::string &str, int width, char fill) noexcept{
+    std::string Temp = str;
+
+    ssize_t strlen = str.size();
+
+    if (width <= strlen)
+    {
+        return Temp;
+    }
+
+    ssize_t left_fill = (width - strlen) / 2;
+    Temp = RJust(Temp, strlen + left_fill, fill);
+    Temp = LJust(Temp, width, fill);
+
+    return Temp;
 }
 
 std::string Replace(const std::string &str, const std::string &old, const std::string &rep) noexcept{
-    // Replace code here
-    return "";
+    std::string result = "";
+    ssize_t strlen = str.size();
+    ssize_t oldlen = old.size();
+
+    if (oldlen == 0)
+    {
+        for (ssize_t i = 0; i < strlen; i ++)
+        {
+            result += rep;
+            result += str[i];
+        }
+        result += rep;
+        return result;
+    }
+
+    for (ssize_t i = 0; i < strlen; )
+    {
+        if (Slice(str, i, i + oldlen) == old)
+        {
+            result += rep;
+            i += oldlen;
+        }
+        else
+        {
+            result += str[i];
+            i ++;
+        }
+    }
+
+    return result;
 }
 
 std::vector< std::string > Split(const std::string &str, const std::string &splt) noexcept{
-    // Replace code here
-    return {};
+    std::vector< std::string > result;
+    ssize_t strlen = str.size();
+    ssize_t spltlen = splt.size();
+
+    if (strlen == 0)
+    {
+        return result;
+    }
+
+    if (spltlen == 0)
+    {
+        for (ssize_t i = 0; i < strlen; i ++)
+        {
+            result.push_back(Slice(str, i, i + 1));
+        }
+        return result;
+    }
+
+    ssize_t start = 0;
+    for (ssize_t i = 0; i < strlen; )
+    {
+        if (Slice(str, i, i + spltlen) == splt)
+        {
+            result.push_back(Slice(str, start, i));
+            i += spltlen;
+            start = i;
+        }
+        else
+        {
+            i ++;
+        }
+    }
+    result.push_back(Slice(str, start, strlen));
+
+    return result;
 }
 
 std::string Join(const std::string &str, const std::vector< std::string > &vect) noexcept{
-    // Replace code here
-    return "";
+    std::string result;
+    size_t vectlen = vect.size();
+
+    for (size_t i = 0; i < vectlen; i ++)
+    {
+        result += vect[i];
+        if (i < vectlen - 1)
+        {
+            result += str;
+        }
+    }
+
+    return result;
 }
 
 std::string ExpandTabs(const std::string &str, int tabsize) noexcept{
-    // Replace code here
-    return "";
+    std::string result;
+    ssize_t strlen = str.size();
+    ssize_t addlen = 0;
+
+    if (tabsize <= 0)
+    {
+        return str;
+    }
+
+    for (ssize_t i = 0; i < strlen; i ++)
+    {
+
+        if (Slice(str, i, i + 1) == "\t")
+        {
+            ssize_t left = tabsize - (addlen % tabsize);
+            for (ssize_t j = 0; j < left; j ++)
+            {
+                result += ' ';
+                addlen ++;
+            }
+        }
+        else
+        {
+            result += str[i];
+            addlen ++;
+        }
+    }
+
+    return result;
 }
 
 int EditDistance(const std::string &left, const std::string &right, bool ignorecase) noexcept{
-    // Replace code here
-    return 0;
+    std::string l = left;
+    std::string r = right;
+    if (ignorecase)
+    {
+        l = Lower(left);
+        r = Lower(right);
+    }
+    ssize_t leftlen = l.size();
+    ssize_t rightlen = r.size();
+
+    std::vector<std::vector<int>> d(leftlen + 1, std::vector<int>(rightlen + 1, 0));
+
+    for (ssize_t i = 0; i <= leftlen; i++)
+    {
+        d[i][0] = i;
+    }
+    for (ssize_t j = 0; j <= rightlen; j++)
+    {
+        d[0][j] = j;
+    }
+
+    for (ssize_t i = 1; i <= leftlen; i++)
+    {
+        for (ssize_t j = 1; j <= rightlen; j++)
+        {
+            int substitutionCost;
+            if (l[i - 1] == r[j - 1])
+            {
+                substitutionCost = 0;
+            }
+            else
+            {
+                substitutionCost = 1;
+            }
+
+            d[i][j] = std::min({d[i - 1][j] + 1, 
+        // Deletion
+            d[i][j - 1] + 1, 
+        // Insertion
+            d[i - 1][j - 1] + substitutionCost});
+        // Substitution
+        }
+    }
+
+    return d[leftlen][rightlen];
 }
 
 };
--- testsrc/StringUtilsTest.cpp
+++ testsrc/StringUtilsTest.cpp
@@ -2,61 +2,100 @@
 #include "StringUtils.h"
 
 TEST(StringUtilsTest, SliceTest){
-    
+    EXPECT_EQ(StringUtils::Slice("Niuhaipeng, Nihao!", 0, 10),std::string("Niuhaipeng"));
+    EXPECT_EQ(StringUtils::Slice("Niuhaipeng, Nihao!", -6, -1),std::string("Nihao"));
+    EXPECT_EQ(StringUtils::Slice("Niuhaipeng, Nihao!", 12, 0),std::string("Nihao!"));
+    EXPECT_EQ(StringUtils::Slice("Niuhaipeng, Nihao!", 20, 25),std::string(""));
+    EXPECT_EQ(StringUtils::Slice("Niuhaipeng, Nihao!", 10, 5),std::string(""));
+    EXPECT_EQ(StringUtils::Slice("", 0, 5),std::string(""));
 }
 
 TEST(StringUtilsTest, Capitalize){
-    EXPECT_EQ(StringUtils::Capitalize("hello"),std::string("Hello"));
+    EXPECT_EQ(StringUtils::Capitalize("niuhaipeng"),std::string("Niuhaipeng"));
+    EXPECT_EQ(StringUtils::Capitalize("nIUHAIPENG"),std::string("Niuhaipeng"));
+    EXPECT_EQ(StringUtils::Capitalize(""),std::string(""));
 }
 
 TEST(StringUtilsTest, Upper){
-    
+    EXPECT_EQ(StringUtils::Upper("niuhaipeng"),std::string("NIUHAIPENG"));
+    EXPECT_EQ(StringUtils::Upper("NIUHAIPENG"),std::string("NIUHAIPENG"));
+    EXPECT_EQ(StringUtils::Upper(""),std::string(""));
 }
 
 TEST(StringUtilsTest, Lower){
-    
+    EXPECT_EQ(StringUtils::Lower("NIUHAIPENG"),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::Lower("niuhaipeng"),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::Lower(""),std::string(""));
 }
 
 TEST(StringUtilsTest, LStrip){
-    
+    EXPECT_EQ(StringUtils::LStrip("  niuhaipeng"),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::LStrip("niuhaipeng  "),std::string("niuhaipeng  "));
+    EXPECT_EQ(StringUtils::LStrip("  niuhaipeng  "),std::string("niuhaipeng  "));
+    EXPECT_EQ(StringUtils::LStrip(""),std::string(""));
 }
 
 TEST(StringUtilsTest, RStrip){
-    
+    EXPECT_EQ(StringUtils::RStrip("  niuhaipeng"),std::string("  niuhaipeng"));
+    EXPECT_EQ(StringUtils::RStrip("niuhaipeng  "),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::RStrip("  niuhaipeng  "),std::string("  niuhaipeng"));
+    EXPECT_EQ(StringUtils::RStrip(""),std::string(""));
 }
 
 TEST(StringUtilsTest, Strip){
-    
+    EXPECT_EQ(StringUtils::Strip("  niuhaipeng  "),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::Strip(""),std::string(""));
 }
 
 TEST(StringUtilsTest, Center){
-    
+    EXPECT_EQ(StringUtils::Center("niuhaipeng", 16, ' '),std::string("   niuhaipeng   "));
+    EXPECT_EQ(StringUtils::Center("niuhaipeng", 16, '0'),std::string("000niuhaipeng000"));
+    EXPECT_EQ(StringUtils::Center("niuhaipeng", 10, ' '),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::Center("", 5, ' '),std::string("     "));
 }
 
 TEST(StringUtilsTest, LJust){
-    
+    EXPECT_EQ(StringUtils::LJust("niuhaipeng", 16, ' '),std::string("niuhaipeng      "));
+    EXPECT_EQ(StringUtils::LJust("niuhaipeng", 16, '0'),std::string("niuhaipeng000000"));
+    EXPECT_EQ(StringUtils::LJust("niuhaipeng", 10, ' '),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::LJust("", 5, ' '),std::string("     "));
 }
 
 TEST(StringUtilsTest, RJust){
-    
+    EXPECT_EQ(StringUtils::RJust("niuhaipeng", 16, ' '),std::string("      niuhaipeng"));
+    EXPECT_EQ(StringUtils::RJust("niuhaipeng", 16, '0'),std::string("000000niuhaipeng"));
+    EXPECT_EQ(StringUtils::RJust("niuhaipeng", 10, ' '),std::string("niuhaipeng"));
+    EXPECT_EQ(StringUtils::RJust("", 5, ' '),std::string("     "));
 }
 
 TEST(StringUtilsTest, Replace){
-    
+    EXPECT_EQ(StringUtils::Replace("niuhaipeng", "i", "I"),std::string("nIuhaIpeng"));
+    EXPECT_EQ(StringUtils::Replace("niuhaipeng", "ip", "IP"),std::string("niuhaIPeng"));
+    EXPECT_EQ(StringUtils::Replace("niuhaipeng", "", "X"),std::string("XnXiXuXhXaXiXpXeXnXgX"));
 }
 
 TEST(StringUtilsTest, Split){
-    
+    EXPECT_EQ(StringUtils::Split("", " "),std::vector<std::string>({}));
+    EXPECT_EQ(StringUtils::Split("niuhaipeng nihao", " "),std::vector<std::string>({"niuhaipeng", "nihao"}));
+    EXPECT_EQ(StringUtils::Split("niuhaipeng nihao", ""),std::vector<std::string>({"n", "i", "u", "h", "a", "i", "p", "e", "n", "g", " ", "n", "i", "h", "a", "o"}));
+    EXPECT_EQ(StringUtils::Split("niuhaipeng,nihao,", ","),std::vector<std::string>({"niuhaipeng", "nihao", ""}));
 }
 
 TEST(StringUtilsTest, Join){
-    
+    EXPECT_EQ(StringUtils::Join(" ", {"niuhaipeng", "nihao"}),std::string("niuhaipeng nihao"));
+    EXPECT_EQ(StringUtils::Join("", {"n", "i", "h", "a", "o"}),std::string("nihao"));
 }
 
 TEST(StringUtilsTest, ExpandTabs){
-    
+    EXPECT_EQ(StringUtils::ExpandTabs("", 4),std::string(""));
+    EXPECT_EQ(StringUtils::ExpandTabs("niuhaipeng\tnihao", 4),std::string("niuhaipeng  nihao"));
+    EXPECT_EQ(StringUtils::ExpandTabs("\tniuhaipeng\tnihao", 4),std::string("    niuhaipeng  nihao"));
+    EXPECT_EQ(StringUtils::ExpandTabs("niuhaipeng\tnihao", 0),std::string("niuhaipeng\tnihao"));
 }
 
 TEST(StringUtilsTest, EditDistance){
-    
+    EXPECT_EQ(StringUtils::EditDistance("niuhaipeng", "nihao"), 6);
+    EXPECT_EQ(StringUtils::EditDistance("niuhaipeng", "niuhaipeng"), 0);
+    EXPECT_EQ(StringUtils::EditDistance("", ""), 0);
+    EXPECT_EQ(StringUtils::EditDistance("Niuhaipeng", "niuhaipeng", true), 0);
 }
